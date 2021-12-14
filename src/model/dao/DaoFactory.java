343a6ffd613b18938e83d4775ae2e11056a3bbb1
phmeyreles <principalphmeyreles3@gmail.com>
2021-12-14 02:57:37 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+// Classe auxiliar responsável por instanciar os daos
+public class DaoFactory {
+
+	// A minha classe vai expor um método que retorna o tipo da interface, mas
+	// internamente ela irá retornar uma implementação
+	
+	// É também uma forma de fazer uma injeção de dependência sem explicitar a implementação
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+}
